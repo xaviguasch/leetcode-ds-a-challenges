b1c9d3761ee783b1c7d963cd0528c8944aacc66a
Xavi Guasch <xaviguasch@gmail.com>
2023-11-30 16:09:50 +0100

    added solution for 0002-squares-of-a-sorted-array using Two Pointers pattern

--- 0002-squares-of-a-sorted-array--EASY-n-977/squares-of-a-sorted-array.js
+++ 0002-squares-of-a-sorted-array--EASY-n-977/squares-of-a-sorted-array.js
@@ -23,10 +23,52 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function (nums) {}
+var sortedSquares = function (nums) {
+  let left = 0
+  let right = nums.length - 1
+
+  const resultArr = []
+
+  while (left <= right) {
+    const squaredLeft = nums[left] ** 2
+    const squaredRight = nums[right] ** 2
+
+    console.log('squaredLeft: ', squaredLeft)
+    console.log('squaredRight ', squaredRight)
+
+    if (squaredLeft >= squaredRight) {
+      console.log('squaredLeft is bigger')
+
+      resultArr.unshift(squaredLeft)
+
+      left++
+    } else {
+      console.log('squaredRight is bigger')
+      resultArr.unshift(squaredRight)
+      right--
+    }
+
+    console.log('resultArr: ', resultArr)
+  }
+
+  return resultArr
+}
 
 console.log(sortedSquares([-4, -1, 0, 3, 10]))
 // Output: [0,1,9,16,100]
 
-console.log(sortedSquares([-7, -3, 2, 3, 11]))
+// console.log(sortedSquares([-7, -3, 2, 3, 11]))
 // Output: [4,9,9,49,121]
+
+// NOTES:
+
+// # Intuition
+// There are several ways of doing it, but "Two Pointers" allows you to do it in O(N) time complexity
+
+// # Approach
+// Implemented Two Pointers pattern
+
+// # Complexity
+// - Time complexity: O(N)
+
+// - Space complexity: O(N) if you take output into account and O(1)O(1)O(1) otherwise.
