de2fd171611dabe7cc5d41705e7f2daebdeb7b99
Xavi Guasch <xaviguasch@gmail.com>
2023-12-02 19:22:05 +0100

    added solution to 0004-is-subsequence

--- 0004-is-subsequence--EASY-n-392/is-subsequence.js
+++ 0004-is-subsequence--EASY-n-392/is-subsequence.js
@@ -24,6 +24,20 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function (s, t) {}
+var isSubsequence = function (s, t) {
+  let i = 0
+  let j = 0
+
+  while (i < s.length && j < t.length) {
+    if (s[i] == t[j]) {
+      i++
+    }
+
+    j++
+  }
+
+  return i == s.length
+}
 
 console.log(isSubsequence('abc', 'ahbgdc'))
+console.log(isSubsequence('"axc"', 'ahbgdc'))
